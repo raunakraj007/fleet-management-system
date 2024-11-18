f23284f245ec26696d49e3b7b48fca680426bdb1
Raunak <kumarraunak16723@gmail.com>
2024-11-18 13:23:25 +0530

    config temp Server

--- frontend/src/components/RoutePage/GetResponseButton.jsx
+++ frontend/src/components/RoutePage/GetResponseButton.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addOptimizeRouteRes } from "../../redux/optimizeRouteRes"
+import { addOptimizeRouteRes } from "../../redux/optimizeRouteRes";
 import axios from "axios";
 
 const BACKEND_URL_ROUTE_OPTIMIZER =
   "http://localhost:8000/api/optimization/fleet-routing/optimize-tours";
-
+const BACKEND_URL_ROUTE_OPTIMIZER2 = "http://localhost:3000/data";
 
 const GetResponseButton = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const shipments = useSelector((state) =>
     state.shipmentSlice.shipments.map(({ id, ...data }) => data)
@@ -43,11 +43,21 @@ const GetResponseButton = () => {
 
     console.log(payLoad);
 
-
-    
-
     axios
       .post(BACKEND_URL_ROUTE_OPTIMIZER, payLoad)
+      .then((response) => {
+        console.log(response);
+        dispatch(addOptimizeRouteRes(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleAPICall2 = () => {
+    console.log("API Call 2");
+    axios
+      .get(BACKEND_URL_ROUTE_OPTIMIZER2)
       .then((response) => {
         console.log(response);
         dispatch(addOptimizeRouteRes(response.data));
@@ -61,7 +71,8 @@ const GetResponseButton = () => {
     <>
       <button
         className=" w-full mt-1 bg-slate-400 active:bg-blue-400"
-        onClick={handleAPICall}
+        // onClick={handleAPICall}
+        onClick={handleAPICall2}
       >
         Get Response
       </button>
